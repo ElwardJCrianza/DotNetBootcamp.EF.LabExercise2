b06520d0f47028f4208f2d774df195aa6ce2db96
ElwardJCrianza <elward.crianza@pointwest.com.ph>
2022-04-26 10:28:45 +0800

    Add project files.

---
+++ Repositories/EmployeeSkillRepository.cs
@@ -0,0 +1,36 @@
+﻿using EntityFramework.LabExercise2.Data;
+using EntityFramework.LabExercise2.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework.LabExercise2.Repositories
+{
+    internal class EmployeeSkillRepository : GenericRepository<EmployeeSkill>, IRepository<EmployeeSkill>
+    {
+        public EmployeeSkillRepository(RecruitmentContext context) : base(context)
+        {
+        }
+        public IEnumerable<dynamic> FilterByEmployeeCode(string employeeCode)
+        {
+            var skills = this.Context.EmployeeSkills
+                .Join(
+                Context.Skills,
+                es => es.CSkillCode,
+                s => s.CSkillCode,
+                (es,s) => new
+                {
+                    CEmployeeCode = es.CEmployeeCode,
+                    CSkillCode = s.VSkill
+                }
+                )
+                .Where(x => x.CEmployeeCode == employeeCode)
+                .ToList();
+            return skills;
+            
+        }
+    }
+}
+
